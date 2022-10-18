fc65e7c5dd901b0a684f9f012d3fe91ee4063811
James Mbere <99666930+mnrx2020@users.noreply.github.com>
2022-10-19 01:01:32 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
-/**
- * prints all characters of a Star Wars movie
- * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
- * Display one character name by line in the same order of the list “characters” in the /films/ response
- */
-
 const request = require('request');
-const id = process.argv[2];
-const endpoint = 'https://swapi-api.hbtn.io/api/films/' + id;
-
-request(endpoint, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    characters.forEach(char => {
-      request(char, function (err, resp, bod) {
-        if (err) console.log(err);
-        else {
-          console.log(JSON.parse(bod).name);
-        }
-      });
-    });
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
